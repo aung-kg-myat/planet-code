dc0cb700b67255fdd98d40c4164417208a137319
AungKaungMyat <mr.aungkaungmyat.dev@gmail.com>
2025-04-17 15:18:20 +0700

    improved SEO

--- app.vue
+++ app.vue
@@ -13,4 +13,11 @@
 </template>
 <script setup lang="ts">
 import { Analytics } from '@vercel/analytics/nuxt';
+useHead({
+  title: 'Planet Code Software Solution',
+  meta: [
+    { name: 'description', content: 'Planet Code Software Solution offers custom software development, web and mobile app development, and innovative IT solutions for businesses worldwide.' },
+    { name: 'keywords', content: 'software development, web development, mobile app development, custom software, IT solutions, software solutions, digital transformation, technology consulting, business automation' }
+  ]
+});
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,12 +12,30 @@ export default defineNuxtConfig({
   app: {
     head: {
       link: [{ rel: "icon", type: "image/png", href: "/logo/planet_code.png" }],
+      meta: [
+        { 
+          name: 'google-site-verification', 
+          content: 'TnIhYEc3y7xR_jUO5BPegYsLjqyLzrZBvie4wlNh3rM' 
+        }
+      ]
     },
   },
-  modules: ["@nuxt/ui", "@nuxt/image", "nuxt-aos", "nuxt3-notifications", "@nuxt/icon"],
+  modules: [
+    "@nuxt/ui",
+    "@nuxt/image",
+    "nuxt-aos",
+    "nuxt3-notifications",
+    "@nuxt/icon",
+    "@nuxtjs/sitemap",
+    "@nuxtjs/robots",
+  ],
   aos: {
     duration: 900, // Global duration for all animations
     easing: "ease",
     once: false, // Set to true if you only want animations to run once
   },
+  site: { 
+    url: 'https://planet-code.vercel.app', 
+    name: 'Planet Code Software Solution' 
+  }, 
 });

9534bca3507c1b0a04b6437cf368776e9c432a2c
AungKaungMyat <mr.aungkaungmyat.dev@gmail.com>
2025-04-25 20:44:00 +0700

    update about

--- components/About.vue
+++ components/About.vue
@@ -25,73 +25,68 @@ const backgroundStyles = computed(() => {
 </script>
 
 <template>
-  <div id="about" class="w-full relative">
+  <div id="about" class="w-full relative h-[1000px] md:h-screen">
     <Gradient />
     <section
-      class="relative w-full h-screen bg-cover bg-center"
+      class="relative w-full h-full bg-cover bg-center"
       style="background-attachment: fixed"
       :style="backgroundStyles"
     >
       <ClientOnly>
         <div
-          class="relative h-[1000px] md:h-screen w-full overflow-hidden bg-background"
+          class="relative w-full h-full overflow-hidden bg-background flex items-center justify-center"
           data-aos="fade-down"
         >
-          <div class="flex items-center justify-center w-full h-full">
-            <div>
-              <div class="flex items-center justify-center gap-2 mb-7">
-                <div class="flex items-center justify-center">
-                  <RadiantText
-                    class="inline-flex items-center justify-center px-4 py-1 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400"
-                    :duration="4"
+          <div>
+            <div class="flex items-center justify-center gap-2 mb-7">
+              <div class="flex items-center justify-center">
+                <RadiantText
+                  class="inline-flex items-center justify-center px-4 py-1 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400"
+                  :duration="4"
+                >
+                  <span class="md:text-5xl text-2xl font-bold font-carterOne"
+                    ><span class="text-white">PLANET</span> CODE</span
                   >
-                    <span class="md:text-5xl text-2xl font-bold font-carterOne"
-                      ><span class="text-white">PLANET</span> CODE</span
-                    >
-                  </RadiantText>
-                </div>
-                <img
-                  src="/logo/planet_code_full.jpeg"
-                  alt="logo"
-                  class="md:w-24 md:h-24 w-16 h-16 rounded-lg"
-                />
+                </RadiantText>
               </div>
-              <h1
-                class="text-center font-bold section-title px-8 font-carterOne"
-              >
-                Your Trusted Partner in Software Development
-              </h1>
-              <p
-                class="font-light font-mono text-justify lg:text-lg text-sm px-8 text-gray-400 max-w-7xl mx-auto"
-              >
-                At
-                <span class="font-carterOne"
-                  >PLANET <span class="!text-[#755dcd]">CODE</span></span
-                >, we are your go-to partner for cutting-edge software
-                development, specializing in crafting innovative solutions for
-                web, mobile, and enterprise platforms. Whether you need a sleek,
-                high-performance website, a robust business management system,
-                or a custom software solution tailored to your unique
-                requirements, we have the expertise to bring your vision to
-                life. <br /><br />
-                At
-                <span class="font-carterOne"
-                  >PLANET <span class="!text-[#755dcd]">CODE</span></span
-                >, we believe that technology should be a catalyst for success.
-                Our approach is deeply collaborative—we take the time to
-                understand your challenges, goals, and ambitions, ensuring that
-                our solutions not only meet your current needs but also empower
-                your long-term vision. With a team of passionate developers,
-                designers, and strategists, we combine creativity with technical
-                excellence to deliver transformative software solutions that
-                help businesses thrive in the digital era. 🚀 Let’s innovate,
-                grow, and redefine possibilities together with
-                <span class="font-carterOne"
-                  >PLANET <span class="!text-[#755dcd]">CODE</span></span
-                >!
-              </p></div
+              <img
+                src="/logo/planet_code_full.jpeg"
+                alt="logo"
+                class="md:w-24 md:h-24 w-16 h-16 rounded-lg"
+              />
+            </div>
+            <h1 class="text-center font-bold section-title px-8 font-carterOne">
+              Your Trusted Partner in Software Development
+            </h1>
+            <p
+              class="font-light font-mono text-justify lg:text-lg text-sm px-8 text-gray-400 max-w-7xl mx-auto"
             >
-          </div>
+              At
+              <span class="font-carterOne"
+                >PLANET <span class="!text-[#755dcd]">CODE</span></span
+              >, we are your go-to partner for cutting-edge software
+              development, specializing in crafting innovative solutions for
+              web, mobile, and enterprise platforms. Whether you need a sleek,
+              high-performance website, a robust business management system, or
+              a custom software solution tailored to your unique requirements,
+              we have the expertise to bring your vision to life. <br /><br />
+              At
+              <span class="font-carterOne"
+                >PLANET <span class="!text-[#755dcd]">CODE</span></span
+              >, we believe that technology should be a catalyst for success.
+              Our approach is deeply collaborative—we take the time to
+              understand your challenges, goals, and ambitions, ensuring that
+              our solutions not only meet your current needs but also empower
+              your long-term vision. With a team of passionate developers,
+              designers, and strategists, we combine creativity with technical
+              excellence to deliver transformative software solutions that help
+              businesses thrive in the digital era. 🚀 Let’s innovate, grow, and
+              redefine possibilities together with
+              <span class="font-carterOne"
+                >PLANET <span class="!text-[#755dcd]">CODE</span></span
+              >!
+            </p></div
+          >
         </div>
       </ClientOnly>
     </section>
